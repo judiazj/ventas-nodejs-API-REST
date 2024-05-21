faf625fb30ac5fcd539fc55c85b5a0c583e6cff1
judiazj <judiazj@unal.edu.co>
2024-05-21 01:04:55 -0500

    Se agregaron casos de prueba para la BD

--- db/cliente.sql
+++ db/cliente.sql
@@ -6,14 +6,15 @@ CREATE TABLE cliente(
     ciudad VARCHAR(15) NOT NULL
 );
 
-INSERT INTO cliente (nombre, direccion, telefono, ciudad) VALUES
-    ('Juan Perez', 'Calle 123', '123456789', 'Medellin'),
-    ('Maria Lopez', 'Avenida 456', '987654321', 'Bogota'),
-    ('Pedro Rodriguez', 'Calle 12', '456123789', 'Cali'),
-    ('Luisa Martinez', 'Carrera 789', '789456123', 'Medellin'),
-    ('Carlos Sanchez', 'Calle 456', '159753468', 'Bogota'),
-    ('Ana Ramirez', 'Avenida 789', '357159246', 'Cali'),
-    ('Miguel Torres', 'Calle Central', '258369147', 'Medellin'),
-    ('Laura Gomez', 'Carrera 123', '147258369', 'Bogota'),
-    ('Daniel Cruz', 'Calle 12', '369147258', 'Cali'),
-    ('Sofia Herrera', 'Avenida 12', '951753852', 'Medellin');
+INSERT INTO cliente VALUES(123,'Simon Bolivar', 'Kra11#9-56', '7702291', 'Cali'),
+        (456,'Mark Zuckerberg', 'Cll 21#95-52', '+57-315291', 'Medellin'),
+        (789,'Drew Barrymore', 'Kra52#65-05', '3125359456', 'Cali'),
+        (741,'Larry Page', 'Cll 05#52-95', '7872296', 'Tunja'),
+        (147,'Tom Delonge', 'Cll 52#65-56', '7992293', 'Medellin'),
+        (852,'Simon Bolivar', 'Kra 21#65-52', '982295', 'Bogota'),
+        (258,'Mark Hoppus', 'Cll 11#95-9', '8952294', 'Bogota'),
+        (963,'Britney Spears', 'Cll 05#52-56', '7705295', 'Tunja'),
+        (369,'John Forbes Nash', 'Kra 21#05-56', '776622966', 'Cali'),
+        (159,'Tom Delonge', 'Kra05#65-05', '6702293','Medellin'),
+        (753,'Sergey Brin', 'Cll 11#65-11', '9702299', 'Medellin'),
+        (153,'Emma Watson', 'Kra 9#9-95', '31569638', 'Tunja');
--- db/producto.sql
+++ db/producto.sql
@@ -1,17 +1,21 @@
 CREATE TABLE producto(
     id_producto SERIAL PRIMARY KEY,
     descripcion VARCHAR(50) NOT NULL, 
-    precio NUMERIC(6,2) NOT NULL CHECK(precio >= 0)
+    precio NUMERIC(8) NOT NULL CHECK(precio >= 0)
 );
 
-INSERT INTO producto (descripcion, precio) VALUES
-    ('Camisa', 20.50),
-    ('Pantalón', 35.75),
-    ('Zapatos', 50.00),
-    ('Gorra', 15.99),
-    ('Bufanda', 10.25),
-    ('Chaqueta', 65.50),
-    ('Vestido', 40.00),
-    ('Traje', 80.25),
-    ('Blusa', 25.75),
-    ('Shorts', 30.99);
+INSERT INTO producto VALUES(1,'Coca-Cola 2L',2400),
+    (2,'Doritos',1000),
+    (3,'Salchicha',3600),
+    (4,'Pan',500),
+    (5,'Queso',1000),
+    (6,'Sandia',8000),
+    (7,'Leche 1L',4563),
+    (8,'Atun',1800),
+    (9,'Pescado',7856),
+    (10,'Cicla Estatica',1800),
+    (11,'Camiseta',12000),
+    (12,'Blue-Jean',7800),
+    (13,'Papaya',1400),
+    (14,'Agua en Bolsa',1800),
+    (15,'Red Bull',1200);
